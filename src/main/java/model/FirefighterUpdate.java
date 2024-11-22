6855579e6185fdd0dd8835b089d9c14cae12137e
Badr <badr.motaim@etu.univ-amu.fr>
2024-11-22 12:00:09 +0100

    Add FirefighterUpdate.java

---
+++ src/main/java/model/FirefighterUpdate.java
@@ -0,0 +1,12 @@
+package model;
+
+public class FirefighterUpdate implements Update{
+    private model.TargetStrategy targetStrategy;
+
+    public FirefighterUpdate() {}
+
+    @Override
+    public void influence(BoardElement element) {
+
+    }
+}
