228365e06f8496679ab208667cb432cd197a5b2a
b21228304 <douaa.benhila@etu.unniv-amu.fr>
2024-11-22 11:16:23 +0100

    add interface BoardState.java

---
+++ src/main/java/model/BoardState.java
@@ -0,0 +1,31 @@
+package model;
+
+import util.Position;
+
+public interface BoardState {
+    /**
+     * Get the state of the board at a specific position.
+     *
+     * @param position The position on the board for which to retrieve the state.
+     * @return The state at the specified position.
+     */
+    BoardElement getState(Position position);
+
+    /**
+     * Set the state of a specific position on the board to the specified state.
+     *
+     * @param state The state to set for the given position.
+     * @param position The position on the board for which to set the state.
+     */
+    void setState(BoardElement state, Position position);
+
+}
+
+
+
+
+
+
+
+
+
