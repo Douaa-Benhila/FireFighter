a4fca8abc07994c80c2f75f69f3348fa27d4ee46
b21228304 <douaa.benhila@etu.unniv-amu.fr>
2024-11-28 15:08:42 +0100

    implement influence()

--- src/main/java/model/FireUpdate.java
+++ src/main/java/model/FireUpdate.java
@@ -1,6 +1,17 @@
 package model;
 
+import util.Position;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FireUpdate implements Update{
     public FireUpdate(){}
-    public void influence(BoardElement element){}
+    public void influence(BoardElement element){
+        List<Position> newFirePositions = new ArrayList<>();
+        for (Position fire : element.getPosition()){
+            newFirePositions.addAll(fire.getNeighbors(fire));
+        }
+        element.getPosition().addAll(newFirePositions);
+    }
 }
