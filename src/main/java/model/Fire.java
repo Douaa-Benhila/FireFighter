82063e33ffb1152d7bc8e5bc9ead151a8d305a94
b21228304 <douaa.benhila@etu.unniv-amu.fr>
2024-11-28 14:33:26 +0100

    IMPLEMENTS THE FIRE.JAVA METHODS

--- src/main/java/model/Fire.java
+++ src/main/java/model/Fire.java
@@ -1,29 +1,55 @@
 package model;
 
 import util.Position;
+
+import java.util.ArrayList;
 import java.util.List;
 
-public class Fire implements BoardElement ,PositionControl{
+public class Fire implements BoardElement ,PositionControl {
     private List<Position> positions;
-    private Update  update;
+    private Update update;
 
-   public Fire(List<Position> positions,Update  update){
-       this.positions=positions;
-       this.update=update;
-   }
-
-    @Override
-    public List<Position> getPosition() { return null;}
+    public Fire(List<Position> positions, Update update) {
+        this.positions = positions;
+        this.update = update;
+    }
 
     @Override
-    public void update() {}
+    public List<Position> getPosition() {
+        return positions;
+    }
+    @Override
+    public void update() {
+        List<Position> modifiedPositions = new ArrayList<>();
+        int step = 0;
+
+        if (step % 2 == 0) {
+            List<Position> newFirePositions = new ArrayList<>();
+            Position[] firePositions;
+            for (Position fire : firePositions) {
+                newFirePositions.addAll(neighbors.get(fire));
+            }
+            firePositions.addAll(newFirePositions);
+            modifiedPositions.addAll(newFirePositions);
+        }
+       ;
+
+    }
 
     @Override
-    public Update getUpdate() {return null;}
+    public Update getUpdate() {
+        return null;
+    }
 
     @Override
-    public void deletePosition(Position position) {}
+    public void deletePosition(Position position) {
+        positions.remove(position);
+    }
 
     @Override
-    public void addPosition(Position position) {}
+    public void addPosition(Position position) {
+        positions.add(position);
+    }
 }
+
+
