fbd3d02591ccce8b2e90c7a55122dc889a56c3e0
Badr <badr.motaim@etu.univ-amu.fr>
2024-11-30 12:44:00 +0100

    Adding MotorizedFirefighter.java class

---
+++ src/main/java/model/MotorizedFirefighter.java
@@ -0,0 +1,37 @@
+package model;
+
+import util.Position;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MotorizedFirefighter implements BoardElement{
+    private List<Position> positions;
+    private FirefighterBoard firefighterBoard;
+    private Update update;
+
+    public MotorizedFirefighter(FirefighterBoard firefighterBoard, Update update, int initialMotorizedFirefighter) {
+        this.firefighterBoard = firefighterBoard;
+        this.update = update;
+        positions = new ArrayList<>();
+        for (int i=0; i<initialMotorizedFirefighter; i++) {
+            positions.add(firefighterBoard.randomPosition());
+        }
+    }
+
+    @Override
+    public List<Position> getPosition() {
+        return positions;
+    }
+
+    @Override
+    public void update() {
+        update.influence(this);
+        update.influence(this);
+    }
+
+    @Override
+    public Update getUpdate() {
+        return update;
+    }
+}
