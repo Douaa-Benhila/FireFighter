6c6638b78e6c083bd9bdba583cb775852823a2c4
Badr <badr.motaim@etu.univ-amu.fr>
2024-11-30 12:51:31 +0100

    Adding the class MotorizedFirefighterFactory.java

---
+++ src/main/java/model/MotorizedFirefighterFactory.java
@@ -0,0 +1,23 @@
+package model;
+
+public class MotorizedFirefighterFactory implements BoardElementFactory{
+    private int initialNumber;
+    private FirefighterBoard firefighterBoard;
+    private Update update;
+
+    public MotorizedFirefighterFactory(int initialNumber, Update update, FirefighterBoard firefighterBoard) {
+        this.initialNumber = initialNumber;
+        this.firefighterBoard = firefighterBoard;
+        this.update = update;
+    }
+
+    @Override
+    public int getInitialNumber() {
+        return initialNumber;
+    }
+
+    @Override
+    public BoardElement createElements() {
+        return new MotorizedFirefighter(firefighterBoard, update, initialNumber);
+    }
+}
