7264e70bdbaaed42888c80067428642a87e11543
douaa <douaa.BENHILA@etu.univ-amu.fr>
2024-12-01 13:42:40 +0100

    add class CloudFactory.java

---
+++ src/main/java/model/CloudFactory.java
@@ -0,0 +1,22 @@
+package model;
+
+public class CloudFactory implements BoardElementFactory{
+    private int initialNumber;
+    private FirefighterBoard firefighterBoard;
+    private Update update;
+
+    public CloudFactory(int initialNumber, Update update, FirefighterBoard firefighterBoard) {
+        this.initialNumber = initialNumber;
+        this.firefighterBoard = firefighterBoard;
+        this.update = update;
+    }
+    @Override
+    public int getInitialNumber() {
+        return initialNumber;
+    }
+
+    @Override
+    public BoardElement createElements() {
+        return new Cloud(firefighterBoard, update, initialNumber);
+    }
+}
