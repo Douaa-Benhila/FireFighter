9538e61ce6e0f996ce091b2b63898630122e0c8c
Badr <badr.motaim@etu.univ-amu.fr>
2024-12-01 12:26:06 +0100

    Adding the class Rockery.java

---
+++ src/main/java/model/Rockery.java
@@ -0,0 +1,40 @@
+package model;
+
+import util.Position;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Rockery implements MotionlessElement{
+    private List<Position> positions;
+    private FirefighterBoard firefighterBoard;
+    private Impact impact;
+
+    public Rockery(Impact impact, int initialMountainCount, FirefighterBoard firefighterBoard) {
+        positions = new ArrayList<>();
+        for (int index = 0; index < initialMountainCount; index++)
+            positions.add(firefighterBoard.randomPosition());
+        this.firefighterBoard = firefighterBoard;
+        this.impact = impact;
+    }
+
+    @Override
+    public List<Position> getPosition() {
+        return positions;
+    }
+
+    @Override
+    public void update() {
+        impact();
+    }
+
+    @Override
+    public Update getUpdate() {
+        return null;
+    }
+
+    @Override
+    public void impact() {
+        impact.impact();
+    }
+}
