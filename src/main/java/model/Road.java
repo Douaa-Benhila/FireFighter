0f4861671c28787e4f8cfb732805c1f74ed2a407
douaa <douaa.BENHILA@etu.univ-amu.fr>
2024-12-01 14:02:25 +0100

    add class Road.java

---
+++ src/main/java/model/Road.java
@@ -0,0 +1,41 @@
+package model;
+
+import util.Position;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Road implements MotionlessElement{
+    private List<Position> positions;
+    private FirefighterBoard firefighterBoard;
+    private Impact impact;
+
+    public Road (Impact impact, int initialMountainCount, FirefighterBoard firefighterBoard) {
+        positions = new ArrayList<>();
+        for (int index = 0; index < initialMountainCount; index++)
+            positions.add(firefighterBoard.randomPosition());
+        this.firefighterBoard = firefighterBoard;
+        this.impact = impact;
+    }
+
+    @Override
+    public List<Position> getPosition() {
+        return positions;
+    }
+
+    @Override
+    public void impact() {
+        impact.impact();
+    }
+
+    @Override
+    public void update() {impact();}
+
+    @Override
+    public Update getUpdate() {
+        return null;
+    }
+
+    public Impact getImpact() {return impact;}
+
+}
