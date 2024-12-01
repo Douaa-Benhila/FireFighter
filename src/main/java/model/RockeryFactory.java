3e16e6135ad247b413d1944f3b62a8f534d9d0df
Badr <badr.motaim@etu.univ-amu.fr>
2024-12-01 13:15:17 +0100

    Adding RockeryFactory.java

---
+++ src/main/java/model/RockeryFactory.java
@@ -0,0 +1,24 @@
+package model;
+
+public class RockeryFactory implements BoardElementFactory{
+    private int initialNumber;
+    private Impact impact;
+    private FirefighterBoard firefighterBoard;
+
+    public RockeryFactory(int initialNumber, Impact impact, FirefighterBoard firefighterBoard) {
+        this.initialNumber = initialNumber;
+        this.impact = impact;
+        this.firefighterBoard = firefighterBoard;
+    }
+
+
+    @Override
+    public int getInitialNumber() {
+        return initialNumber;
+    }
+
+    @Override
+    public BoardElement createElements() {
+        return new Rockery(impact, initialNumber, firefighterBoard);
+    }
+}
