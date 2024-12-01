424255a1caf93cf33074f5c9387c1f1e47ece39c
douaa <douaa.BENHILA@etu.univ-amu.fr>
2024-12-01 13:39:54 +0100

    add class Cloud.java

---
+++ src/main/java/model/Cloud.java
@@ -0,0 +1,37 @@
+package model;
+
+import util.Position;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cloud implements BoardElement {
+    private List<Position> positions;
+    private FirefighterBoard firefighterBoard;
+    private Update update;
+
+    public Cloud(FirefighterBoard firefighterBoard, Update update, int initialCloud) {
+        this.firefighterBoard = firefighterBoard;
+        this.update = update;
+        positions = new ArrayList<>();
+        for (int i=0; i<initialCloud; i++) {
+            positions.add(firefighterBoard.randomPosition());
+        }
+    }
+    @Override
+    public List<Position> getPosition() {
+        return positions;
+    }
+
+    @Override
+    public void update() {
+        update.influence(this);
+        update.influence(this);
+    }
+
+    @Override
+    public Update getUpdate() {
+        return update;
+    }
+
+}
