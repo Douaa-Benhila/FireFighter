738312762084af5b363b0dd8e8455faa90726809
douaa <douaa.BENHILA@etu.univ-amu.fr>
2024-12-01 13:41:40 +0100

    add class CloudUpdate.java

---
+++ src/main/java/model/CloudUpdate.java
@@ -0,0 +1,50 @@
+package model;
+
+import util.Position;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CloudUpdate implements Update  {
+    private model.TargetStrategy targetStrategy;
+    private FirefighterBoard firefighterBoard;
+
+    public CloudUpdate(FirefighterBoard firefighterBoard) {
+        targetStrategy = new model.TargetStrategy();
+        this.firefighterBoard = firefighterBoard;
+    }
+    @Override
+    public void influence(BoardElement element) {
+        List<Position> newCloudPositions = new ArrayList<>();
+        for (Position cloudPosition : element.getPosition()) {
+            Position newCloudPosition = firefighterBoard.randomPosition();
+            newCloudPositions.add(newCloudPosition);
+            extinguish(newCloudPosition);
+            List<Position> neighborFirePositions = firefighterBoard.getNeighbors()
+                    .get(newCloudPosition)
+                    .stream()
+                    .filter(getFirePositions()::contains)
+                    .toList();
+            for (Position firePosition : neighborFirePositions) {
+                extinguish(firePosition);
+            }
+        }
+        element.getPosition().clear();
+        element.getPosition().addAll(newCloudPositions);
+    }
+
+    private List<Position> getFirePositions(){
+        for (BoardElement element:firefighterBoard.getElements()){
+            if(element instanceof Fire) return element.getPosition();
+        }
+        return null;
+    }
+
+    private void extinguish(Position position) {
+        getFirePositions().remove(position);
+    }
+
+
+
+
+}
