6fbad3d4eb5c1d9c6bfedd4e83d760c992421932
b21228304 <douaa.benhila@etu.unniv-amu.fr>
2024-12-03 14:36:58 +0100

    changing coulor of cloud to purple to be more clear in the GUI while execution

--- src/main/java/view/ViewElement.java
+++ src/main/java/view/ViewElement.java
@@ -3,7 +3,7 @@ package view;
 import javafx.scene.paint.Color;
 
 public enum ViewElement {
-  FIREFIGHTER(Color.BLUE), MOTORIZEDFIREFIGHTER(Color.ORANGE),FIRE(Color.RED), ROCKERY(Color.GREEN),MOUNTAIN(Color.DARKGRAY),EMPTY(Color.WHITE),CLOUD(Color.LIGHTGRAY),ROAD(Color.BLACK);
+  FIREFIGHTER(Color.BLUE), MOTORIZEDFIREFIGHTER(Color.ORANGE),FIRE(Color.RED), ROCKERY(Color.GREEN),MOUNTAIN(Color.DARKGRAY),EMPTY(Color.WHITE),CLOUD(Color.PURPLE),ROAD(Color.BLACK);
   final Color color;
   ViewElement(Color color) {
     this.color = color;
