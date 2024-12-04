3507094f36fb6ad69bc526769f5c5a6b2755f719
Badr <badr.motaim@etu.univ-amu.fr>
2024-12-04 14:09:43 +0100

    Fixed a problem in RockeryImpact.java. All Elements are working as they are expected to

--- src/main/java/model/RockeryImpact.java
+++ src/main/java/model/RockeryImpact.java
@@ -2,29 +2,33 @@ package model;
 
 import util.Position;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class RockeryImpact implements Impact{
     private FirefighterBoard firefighterBoard;
-    private Map<Position, Integer> fireFreezeCounter;
+    private Map<Position, Integer> fireCounters;
 
     public RockeryImpact(FirefighterBoard firefighterBoard) {
         this.firefighterBoard = firefighterBoard;
-        this.fireFreezeCounter = new HashMap<>();
+        this.fireCounters = new HashMap<>();
     }
 
     @Override
     public void impact() {
-        for (List<Position> neighbors : firefighterBoard.getNeighbors().values()) {
-            for (Position neighbor : neighbors) {
+        for (Map.Entry<Position, List<Position>> entry : firefighterBoard.getNeighbors().entrySet()) {
+            for (Position neighbor : entry.getValue()) {
                 if (getRockeryPositions().contains(neighbor)) {
-                    List<BoardElement> elements = firefighterBoard.getState(neighbor);
+                    List<BoardElement> elements = firefighterBoard.getState(entry.getKey());
                     for (BoardElement element : elements) {
                         if (element instanceof Fire) {
-                            fireFreeze((Fire) element, neighbor);
+                            int currentFireStep = fireCounters.getOrDefault(neighbor, 0);
+
+                            if (currentFireStep >= 4) {
+                                getFirePositions().add(neighbor);
+                                getRockeryPositions().remove(neighbor);
+                            } else {
+                                fireCounters.put(neighbor, currentFireStep + 1);
+                            }
                         }
                     }
                 }
@@ -32,26 +36,6 @@ public class RockeryImpact implements Impact{
         }
     }
 
-    private void fireFreeze(Fire fire, Position position) {
-        fireFreezeCounter.putIfAbsent(position, 0);
-        int freezeCount = fireFreezeCounter.get(position);
-
-        if (freezeCount < 4) {
-            fireFreezeCounter.put(position, freezeCount + 1);
-        } else {
-            propagateFire(position);
-            fireFreezeCounter.remove(position);
-        }
-    }
-
-    private void propagateFire(Position position) {
-        List<Position> neighbors = firefighterBoard.getNeighbors().get(position);
-        for (Position neighbor : neighbors) {
-            if (!getRockeryPositions().contains(neighbor)) {
-                getFirePositions().add(neighbor);
-            }
-        }
-    }
 
     private List<Position> getRockeryPositions(){
         for (BoardElement element:firefighterBoard.getElements()){
